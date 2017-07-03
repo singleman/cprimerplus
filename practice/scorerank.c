acd227c9c4e3710f0645467fe5c544d8bbc90600
singleman <singleman.real@163.com>
2017-07-04 07:31:35 +0800

    7-4

--- practice/scorerank.c
+++ practice/scorerank.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include<stdio.h> 
 #include<stdlib.h>
 #include<string.h>
+#include<ctype.h>
 
 #define LEN 40
 
@@ -25,20 +26,65 @@ int main()
 	struct rank *pt;
 	pt=malloc(sizeof(struct rank));
 	*/
-	int count,Maxsize;
+
+
+	int i,count,Maxsize;
+	char option;
+
 	Maxsize=3;
-	struct student *pt
-	pt=Maxsize*malloc(sizeof(struct student));
+	struct student *pt,*temp;
+	pt=malloc(Maxsize*sizeof(struct student));
+	temp=malloc(sizeof(struct student));
 
 	
-	printf("please input the student name:\n");	
-	while(s_get((pt->stu[0]).name,LEN) != NULL)
-		printf("%s\n",(pt->stu[0]).name);
+	//while(s_get((pt->stu[0]).name,LEN) != NULL)
+	for(i=0;i<Maxsize;i++)
+	{
+		printf("please input the student name:\n");	
+		s_get((pt+i)->name,LEN);   // pt[0].name (pt+1)->name
+					//	printf("%s\n",pt[0].name);
+		
+			printf("please input the score:\n");	
+			scanf("%d %d %d",&((pt+i)->liter),&((pt+i)->math),&((pt+i)->english));
+			(pt+i)->total=(pt+i)->liter + (pt+i)->math + (pt+i)->english;
+	
+		printf("name: %4s; liter: %3d; math: %3d; english: %3d; total: %3d\n",(pt+i)->name,(pt+i)->liter,(pt+i)->math,(pt+i)->english,(pt+i)->total);
+		while(getchar()!='\n')
+			continue;
+	}
+
+	printf("please choose the key to sort\n:");
+	//while((option=getchar())!='*')
+	scanf("%c",&option);
+	{
+		//if( option=='\n')
+		//	continue;
+		if(islower(option))
+			switch(option)
+			{
+			case 't':
+				{
+				for(i=0;i<Maxsize-1;i++)
+					if((pt+i)->total > (pt+i+1)->total)
+					{
+						*temp=*(pt+i);
+						*(pt+i)=*(pt+i+1);
+						*(pt+i+1)=*temp;
+					}
+				}
 
 
+			}
+
+	}
 
 
+	for(i=0;i<Maxsize;i++)
+		printf("name: %4s; liter: %3d; math: %3d; english: %3d; total: %3d\n",(pt+i)->name,(pt+i)->liter,(pt+i)->math,(pt+i)->english,(pt+i)->total);
 
+
+	free(pt);
+	free(temp);
 	return 0;
 }
 
@@ -51,7 +97,7 @@ char *s_get(char *st, int n)
 	{
 		find = strchr(st,'\n');
 		if(find)
-			*find = '\n';
+			*find = '\0';
 		else
 			while(getchar() != '\n')
 				continue;
