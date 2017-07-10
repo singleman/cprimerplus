50ee0551ac7b24f03cb1ce5e0866c9f73bf9f221
singleman <singleman.real@163.com>
2017-07-10 22:47:21 +0800

    7-10-2

--- practice/queen.c
+++ practice/queen.c
@@ -2,16 +2,21 @@
 
 #define LEN 8
 
-int cal(int row, int col, int (*pos)[2]);
 void pri_position(int (*pos)[2]);
+int cal(int row, int col, int (*pos)[2]);
+int count=0;
 
 int main()
 {
 //	char check_board[LEN][LEN];
 //	char result[LEN][LEN];
+
 	int position[LEN][2];
-	cal(0,0,position);
-	pri_position(position);
+	int i;
+
+//	for(i=0;i<LEN;i++)
+		cal(0,i,position);
+		printf("%d\n",count);
 
 
 
@@ -30,24 +35,26 @@ void pri_position(int (*pos)[2])
 {
 	int i;
 	for(i=0;i<LEN;i++)
-		printf("%d %d\n",pos[i][1],pos[i][2]);
+		printf("%d %d	",pos[i][0],pos[i][1]);
 }
 
 int cal(int row,int col,int (*pos)[2])
 {
 	int j,critical;
-	if(row==0)
-	{
-		pos[row][1]=row;
-		pos[row][2]=col;
-		cal(row+1,0,pos);
-	}
 
 	for(j=col;j<LEN;j++)  //row 0-7
 	{
+		critical=1;
+		/*if(row==0)
+		{
+			pos[row][0]=row;
+			pos[row][1]=j;
+			//cal(row+1,0,pos);
+		}
+		*/
 		for(int i=0;i<row;i++)
 		{
-			if(row!=pos[i][1] && j!=pos[i][2] && (row+j)!=(pos[i][1]+pos[i][2]) && (row-j)!=(pos[i][1]-pos[i][2]))
+			if(row!=pos[i][0] && j!=pos[i][1] && (row+j)!=(pos[i][0]+pos[i][1]) && (row-j)!=(pos[i][0]-pos[i][1]))
 				critical=1;
 			else
 			{
@@ -58,19 +65,25 @@ int cal(int row,int col,int (*pos)[2])
 		}
 		if(critical)
 		{
-			pos[row][1]=row;
-			pos[row][2]=j;
+			pos[row][0]=row;
+			pos[row][1]=j;
 			if(row==(LEN-1))
+			{
+				pri_position(pos);
+				count++;
+				printf("\n");
 				return 0;
+			}
 			cal(row+1,0, pos);
 		}
 		else
 			continue;
 	}
-	if(pos[row-1][2]+1<LEN)
-		cal(row-1,pos[row-1][2]+1,pos);
+	/*if(pos[row-1][1]+1<LEN)
+		cal(row-1,pos[row-1][1]+1,pos);
 	else
-		cal(row-2,pos[row-2][2]+1,pos);
+		cal(row-2,pos[row-2][1]+1,pos);
+		*/
 }
 
 
