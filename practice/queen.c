2fc076ae67da424c4861dfb0c44e7bf5b86d9b52
singleman <singleman.real@163.com>
2017-07-11 06:18:36 +0800

    7-11-2

--- practice/queen.c
+++ practice/queen.c
@@ -12,26 +12,21 @@ int main()
 //	char result[LEN][LEN];
 
 	int position[LEN][2];
-	int i;
-
-//	for(i=0;i<LEN;i++)
-		cal(0,i,position);
-		printf("%d\n",count);
-
-
+	cal(0,0,position);
+	printf("%d\n",count);
 
 	return 0;
 }
 
 /*
-void output(char (*pt)[LEN])
+void output(char (*pt)[LEN])  //printf check_board
 {
 
 }
 
 */
 
-void pri_position(int (*pos)[2])
+void pri_position(int (*pos)[2])   //printf each possibility
 {
 	int i;
 	for(i=0;i<LEN;i++)
@@ -42,7 +37,7 @@ int cal(int row,int col,int (*pos)[2])
 {
 	int j,critical;
 
-	for(j=col;j<LEN;j++)  //row 0-7 don't break;
+	for(j=col;j<LEN;j++)  //col traversal 0-7; don't break;
 	{
 		critical=1;
 		/*if(row==0)
@@ -52,14 +47,14 @@ int cal(int row,int col,int (*pos)[2])
 			//cal(row+1,0,pos);
 		}
 		*/
-		for(int i=0;i<row;i++)
+		for(int i=0;i<row;i++)   //find whether it is contradictiory
 		{
 			if(row!=pos[i][0] && j!=pos[i][1] && (row+j)!=(pos[i][0]+pos[i][1]) && (row-j)!=(pos[i][0]-pos[i][1]))
 				critical=1;
 			else
 			{
 				critical=0;
-				break;
+				break; 
 			}
 
 		}
@@ -82,7 +77,7 @@ int cal(int row,int col,int (*pos)[2])
 			continue;
 	}
 	  if(pos[row-1][1]+1<LEN)
-		cal(row-1,pos[row-1][1]+1,pos);
+		cal(row-1,pos[row-1][1]+1,pos);//Do not need to manually traverse
 	else
 		cal(row-2,pos[row-2][1]+1,pos);
 		*/
