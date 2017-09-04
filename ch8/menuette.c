919273250f173fe7808e524309ec69a911a4b22f
singleman <singleman.real@163.com>
2017-09-05 06:58:50 +0800

    9-5

---
+++ ch8/menuette.c
@@ -0,0 +1,32 @@
+#include<stdio.h>
+
+char get_choice(void);
+char get_first(void);
+int get_int(void);
+void count(void);
+
+int main(void)
+{
+	int choice;
+	void count(void);
+
+	while( (choice = get_choice()) != 'q')
+	{
+		case 'a':
+			printf("1\n");
+			break;
+		case 'b':
+			printf("2\n");
+			break;
+		case 'c':
+			count();
+			break;
+		default :
+			printf("d\n");
+			break;
+	}
+	printf("bye\n");
+
+	return 0;
+}
+
