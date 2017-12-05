74652244a46771833d6c54ba097b4f19886ff3c7
singleman <singleman.real@163.com>
2017-12-05 17:26:42 +0800

    1205

--- practice/N2.c
+++ practice/N2.c
@@ -1,5 +1,5 @@
 #include<stdio.h>
-#define LEN 4
+#define LEN 20 
 int main()
 {
     int i,j,k,N,key,content;
@@ -9,47 +9,50 @@ int main()
     content=1;
 
     //scanf("%d",&N);
-    N=LEN;
-    for(k=N-1;k<=1-N;k--)
+    
+    for(N=1;N<7;N++)
     {
-        key++;
-        content=1;
-        if(k>=0)
-        {
-            switch(key%2)
-            {
-            case 1:
-                for(i=N-1,j=N-1-k;j>=0;i--,j--)
-                    matrix[i][j]=content++;
-                break;
-            case 0:
-                for(j=0,i=k;i<N;i++,j++)
-                    matrix[i][j]=content++;
-                break;
-            }
-        }
-        else
-        {
-          switch(key%2)
-            {
-            case -1:
-                for(j=N-1,i=N-1+k;i>=0;i--,j--)
-                    matrix[i][j]=content++;
-                break;
-            case 0:
-                for(i=0,j=-k;j<N;i++,j++)
-                    matrix[i][j]=content++;
-                break;
-            }
-        }
+	content=1;
+   	for(k=N-1;k>=1-N;k--)
+   	{
+       		key++;
+      		if(k>=0)
+      		{
+         		switch(key%2)
+          		{
+          			case 1:
+        				for(i=N-1,j=N-1-k;j>=0;i--,j--)
+             					matrix[i][j]=content++;
+					break;
+           			case 0:
+               				for(j=0,i=k;i<N;i++,j++)
+						matrix[i][j]=content++;
+               				break;
+            		}
+       		}
+       		else
+       		{
+        		switch(key%2)
+         		{
+         			case 1:
+               				for(j=N-1,i=N-1+k;i>=0;i--,j--)
+                   				matrix[i][j]=content++;
+               				break;
+            			case 0:
+               				for(i=0,j=-k;j<N;i++,j++)
+                  				matrix[i][j]=content++;
+               				break;
+           		}
+                 }
+   	 }
+	printf("N=%d,the matrix is:\n",N);
+    	for(i=0;i<N;i++)
+    	{
+		for(j=0;j<N;j++)
+        		printf("%4d",matrix[i][j]);
+      		printf("\n");
+   	}
     }
-    for(i=0;i<N;i++)
-    {
-        for(j=0;j<N;j++)
-            printf("%4d",matrix[i][j]);
-        printf("\n");
-    }
-
 
     return 0;
 }
